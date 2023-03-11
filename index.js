386cc9a5bdacf408eb1319592ea32529b9573270
Jessica Carreon <jessica990790@yahoo.com>
2023-03-10 23:48:59 -0700

    calc-challenge-lab

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b){
+    return a+b;
+}
+function subtract(a, b) {
+    return a-b;
+}
+function multiply(a, b) {
+    return a*b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment (n) {
+    return (n += 1)
+}
+
+function decrement (n) {
+    return (n -= 1)
+}
+function makeInt(string, base) {
+    return parseInt(n, 10)
+}
+function makeInt(NaN, base) {
+    return parseInt(n, 10)
+}
+function makeInt(nonsense){
+    return nonsense
+}
+function makeInt(string){
+    return parseInt(string,10)
+}
+function preserveDecimal (n) {
+    return parseFloat (n)
+}
